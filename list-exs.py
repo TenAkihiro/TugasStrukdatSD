8f46c9c6d402713b72650ca4aceb933eabe799d4
TenAkihiro <hafizuldamarlaya@gmail.com>
2021-12-24 13:42:44 +0700

    ya

---
+++ list-exs.py
@@ -0,0 +1,32 @@
+# list pada python
+# Contoh
+# int A = [] # array
+alpabet = [] # definis standar dari python
+alpabet.append('A');
+alpabet.append('C');
+alpabet.append('B');
+print(alpabet)
+alpabet.sort()
+print(alpabet)
+
+
+# fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+# print(fruits)
+
+# print(fruits.count('apple'))
+# print(fruits.count('tangerine'))
+# print(fruits.index('banana'))
+# print(fruits.index('banana', 4))  # Find next banana starting a position 4
+
+# fruits.reverse()
+# print(fruits)
+
+# fruits.append('grape')
+# fruits.append('manggo')
+# print(fruits)
+
+# fruits.sort()
+# print(fruits)
+
+# fruits.pop()
+# print(fruits)
